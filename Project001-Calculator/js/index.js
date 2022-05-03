6467aea92a36347583ffbcd8704a531d25641da0
mikeoconnor20 <mikeoconnor20@gmail.com>
2022-05-02 20:56:51 -0400

    Update index.js

--- Project001-Calculator/js/index.js
+++ Project001-Calculator/js/index.js
@@ -1,40 +1,26 @@
+let display = document.getElementById("display")
 
-let currentAnswer = []
+// update display
+document.querySelectorAll('.buttons').forEach(item => {
+    item.addEventListener('click', event => {
+      var numberClicked = event.target.innerText
+      display.innerText += numberClicked
+    })
+  })
 
+//   clear
+document.getElementById("clear").addEventListener("click", function clear(e) {
+    display.innerText = ""
+})
 
-calculatorbody = document.querySelectorAll("li")
+// equals
+document.getElementById("equalsButton").addEventListener("click", function problemEquals(e) {
+    const answer = eval(display.innerText.slice(0, length-1));
+    console.log(answer);
+    display.innerText = answer;
+} )
 
-calculatorbody.forEach((item) => {
-
-  item.addEventListener("click", function(){
-    let currentButton = item.innerText
-
-    if (!item.classList.contains("operator")) {
-      currentButton = Number(currentButton)
-    }
-
-
-    currentAnswer.push(currentButton)
-
-    console.log(currentAnswer)
-
-
-    if (currentButton === "=") {
-      equal = (x,y,z)=> {
-        x = currentAnswer[0];
-        z = currentAnswer[2];
-
-        if (y === "+") {
-          a = x+z
-
-        }
-        let a = x+z
-        console.log(a)
-
-      }
-      console.log(equal())
-    }
-
-
-  });
-});
+// backspace *** i did length-2 to delete the arrow and the most recent input.
+document.getElementById("backspace").addEventListener("click", function backspace(e) {
+    display.innerText = display.innerText.slice(0, length-2)
+})
